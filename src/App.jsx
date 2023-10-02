9c415dff1367269132b26ea24261e27a9122f40f
Luqman Nasution Abdul Hamid <nasutionsavier@gmail.com>
2023-10-02 18:26:27 +0800

    add all changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Luqman Nasution",
+  title: "Microsoft Learn Student Ambassador / Executive Vice Treasurer of the Student Representative Council UiTM Johor",
+  email: "Aliff.Nasution@studentambassadors.com",
+  gitHub: "Luqman Nasution Abdul Hamid",
+  instagram: "",
+  linkedIn: "Luqman Nasution Abdul Hamid",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/asli.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "Picture of Luqman Nasution smiling at the aboriginal people.";
 
 /**
  * Project list
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Featured in Local Newspaper👏",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "Featured in Local Newspaper",
+    url: "https://www.nst.com.my/news/nation/2020/07/606401/students-win-3-categories-international-stem-competition",
+    },
+  {
+    title: " My LinkedIn Profile",
+    description:
+      "My LinkedIn profile where I share my experiences and connect with others in the community.",
+    url: "https://www.linkedin.com/in/luqmannasution/",
   },
   {
-    title: "Web Development for Beginners",
+    title: "CHEPAT Initiatives",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "A student-led non-profit initiative that aims to provide aid for the UiTM Pasir Gudang Chemical Engineering students.",
+    url: "https://www.linkedin.com/company/chepat2023/",
   },
   {
-    title: "My Resume Site",
+    title: "CERYA Project",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
-  },
-  {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "A youth-led organisation aspiring to bridge Mental Health and Sex Education among youths.",
+    url: "https://www.linkedin.com/company/ceria-project-movement/",
   },
 ];
 
